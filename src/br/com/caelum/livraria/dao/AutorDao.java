1d3fb823835efd27dc53531fb8cc8965b8371f3f
Aldecir Neves de Lima <aldecirlima@gmail.com>
2022-06-30 21:58:23 -0300

    Aula 4 concluída

--- src/br/com/caelum/livraria/dao/AutorDao.java
+++ src/br/com/caelum/livraria/dao/AutorDao.java
@@ -4,39 +4,46 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.Stateless;
+import javax.ejb.TransactionManagement;
+import javax.ejb.TransactionManagementType;
+import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.transaction.UserTransaction;
 
 import br.com.caelum.livraria.modelo.Autor;
 
 // A anotação @Stateless transforma a Classe DAO em um EJB.
 
 @Stateless
+@TransactionManagement(TransactionManagementType.BEAN) // opcional
 public class AutorDao {
 
 //	No EJB a anotação de injeção do EntityManager é @PersistenceContext
 	@PersistenceContext
 	private EntityManager manager;
 	
+	@Inject
+	UserTransaction tx;
+
 	@PostConstruct
 	void aposCriacao() {
-	    System.out.println("AutorDao foi criado.");
+		System.out.println("AutorDao foi criado.");
 	}
 
 	public void salva(Autor autor) {
 		System.out.println("Salvando Autor " + autor.getNome());
-		
-//		try {
-//			Thread.sleep(20000); //20s
-//		} catch (InterruptedException e) {
-//			e.printStackTrace();
-//		}
-		
+
+		try {
+			tx.begin();
 		manager.persist(autor);
-		
+			tx.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		System.out.println("Salvou Autor " + autor.getNome());
 	}
-	
+
 	public List<Autor> todosAutores() {
 		return manager.createQuery("SELECT a FROM Autor a", Autor.class).getResultList();
 	}
@@ -45,5 +52,5 @@ public class AutorDao {
 		Autor autor = this.manager.find(Autor.class, autorId);
 		return autor;
 	}
-	
+
 }
